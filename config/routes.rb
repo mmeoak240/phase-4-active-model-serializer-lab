248360db6a53efc3c93a6b5aa5c9db033cda9c10
Michael Meoak <mmeoak240@gmail.com>
2022-08-28 18:04:24 -0700

    commit tree 0879e55aca64b6a1ecf4f31ec935b06831ee9faf

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   resources :tags, only: [:index, :show]
   resources :posts, only: [:index, :show]
   resources :authors, only: [:index, :show]
+
+  get '/authors/:id/author_profile', to: 'authors#author_profile'
+  get '/posts/:id', to: 'post#postContent'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
